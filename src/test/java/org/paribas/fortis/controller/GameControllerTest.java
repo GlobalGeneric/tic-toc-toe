52323d43f0ba79da1a795f79f2dd31049cc26855
GlobalGeneric <akbarpouramir@gmail.com>
2023-02-06 19:45:49 +0330

    Mock SimpMessagingTemplate bean

--- src/test/java/org/paribas/fortis/controller/GameControllerTest.java
+++ src/test/java/org/paribas/fortis/controller/GameControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -47,6 +48,9 @@ public class GameControllerTest {
 
     @MockBean
     GameRepository gameRepository;
+
+    @MockBean
+    SimpMessagingTemplate SimpMessagingTemplate;
 
     @Test
     void post_palyer_object_should_return_Status_OK() throws Exception {
